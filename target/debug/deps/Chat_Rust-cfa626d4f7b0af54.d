b1593e3564bcc028863e08653ff6242e474dadfa
Bilel Mahli <bilel.mahli@hotmail.com>
2024-05-16 14:14:38 +0200

    First commit

---
+++ target/debug/deps/Chat_Rust-cfa626d4f7b0af54.d
@@ -0,0 +1,5 @@
+C:\Users\USER\Chat_Rust\target\debug\deps\libChat_Rust-cfa626d4f7b0af54.rmeta: src/main.rs
+
+C:\Users\USER\Chat_Rust\target\debug\deps\Chat_Rust-cfa626d4f7b0af54.d: src/main.rs
+
+src/main.rs:
